aa828371843d0f21a870f0a8a0cc7d94cc538639
kunal-jain-99 <jainkunallalit@gmail.com>
2019-10-22 18:53:26 +0530

    Updated the main file

--- takeabreak.py
+++ takeabreak.py
@@ -1,10 +1,18 @@
 import time
-import webbrowser
+import webbrowser, os
 
 total_breaks = int(input("How many Breaks Do you Want? "))
 break_count = 0
+break_time = int(input("How long do you want the break to be?(in minutes) "))
+work_time = int(input("How long to wait between work and break?(in minutes) "))
+print("You can now minimize this Window")
 
 while break_count < total_breaks:
-	time.sleep(3600)	#wait 1 hour 
+	time.sleep(work_time*60)	#wait for specified time 
 	webbrowser.open("https://www.youtube.com/watch?v=ApgNm4Bu-Lw")	#open browser with the specified link
+	time.sleep(break_time*60)	#wait for specified time
+	# os.system("taskkill /im firefox.exe /f")
+	# os.system("taskkill /im opera.exe /f")
+	# os.system("taskkill /im chrome.exe /f")	#autokill task after few seconds to get back to work
+	webbrowser.open("http://novascjj.com/wp-content/uploads/2014/12/back-to-work.jpg")
 	break_count += 1	#increment break_count by 1
